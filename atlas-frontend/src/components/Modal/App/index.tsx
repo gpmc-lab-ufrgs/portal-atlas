797eaf5607ccfc2b696bd7b5899eab1ef9c09b4a
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-02-22 09:06:59 -0300

    Minor modification to folder organization, deleted the App.tsx arquiver and puted the code in index.tsx

--- atlas-frontend/src/components/Modal/App/index.tsx
+++ atlas-frontend/src/components/Modal/App/index.tsx
@@ -1,1 +1,23 @@
-export { default } from './App';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/data/') // Replace this URL with your API endpoint
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div style={{ width: '100%', height: '100vh', padding: '100px' }}>
+      <h4>Conteúdo da API /data</h4>
+      <p>State: {data.state}</p>
+      <p>District: {data.district}</p>
+      {/* Render the rest of your content */}
+    </div>
+  );
+}
+
+export default App;
