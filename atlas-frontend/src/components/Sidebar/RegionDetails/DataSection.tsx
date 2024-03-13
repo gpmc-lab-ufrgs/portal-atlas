31647ec13caadc8dd287ffcd34e0db25eae46d03
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-13 08:38:40 -0300

    Removed unused imports and unnecessary comments in DataSection.tsx

--- atlas-frontend/src/components/Sidebar/RegionDetails/DataSection.tsx
+++ atlas-frontend/src/components/Sidebar/RegionDetails/DataSection.tsx
@@ -1,15 +1,6 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
 import Collapsible from '@components/Collapsible';
-import MetricDetails from '@components/MetricDetails';
-import { useSelectedDistrict } from '@context/district/selectedContext';
-import { useSelectedState } from '@context/state/selectedContext';
-import { useComparison } from '@context/comparisonContext';
-import { useComparison as useComparisonState } from '@context/comparisonContextState';
-import { MapPropsContentType, MapPropsSectionType } from '@customTypes/map';
-import { Tooltip } from '@mui/material';
 import { CollapsibleContent } from './CollapsibleContent';
-import * as Styles from './styles';
 import { Estado } from 'src/interfaces/Estado.type';
 import { Cidades } from 'src/interfaces/Cidades.type';
 
