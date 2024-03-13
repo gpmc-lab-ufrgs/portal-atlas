0a8d214af358931ae574d4a6cbe6492baf399534
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-13 08:38:58 -0300

    Refactor Component RegionDetails

--- atlas-frontend/src/components/Sidebar/RegionDetails/index.tsx
+++ atlas-frontend/src/components/Sidebar/RegionDetails/index.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
-
 import { useAppSelector } from '@hook/hooks';
 import { estadoSelected } from 'src/features/estadoSlice';
 import { cidadeSelected } from 'src/features/cidadeSlice';
-
 import ComparisonSection from './ComparisonSection';
 import DataSection from './DataSection';
 import { useLocation } from 'react-router-dom';
@@ -13,41 +10,38 @@ import { Estado } from 'src/interfaces/Estado.type';
 import { Cidades } from 'src/interfaces/Cidades.type';
 
 const RegionDetails = () => {
-  const isStateSel = window.location.href.includes('/state');
-  const isDistrictSel = window.location.href.includes('/district');
+  const isStateSelected = window.location.href.includes('/state');
+  const isDistrictSelected = window.location.href.includes('/district');
+
   const selectedEstado = useAppSelector(estadoSelected);
   const selectedCidade = useAppSelector(cidadeSelected);
-  const [lstDadosEstado, setLstDadosEstado] = useState<Estado[]>([]);
-  const [lstDadosCidade, setLstDadosCidade] = useState<Cidades[]>([]);
+
+  const [lstDados, setLstDados] = useState<Estado[] | Cidades[]>([]);
   const [lstDistinct, setLstDistinct] = useState<string[]>([]);
+
   const location = useLocation();
   const { pathname } = location;
+
   const isEnglish = pathname.includes('/en');
 
   useEffect(() => {
-    if (isStateSel) {
-      setLstDadosEstado(selectedEstado);
-      let t = isEnglish ? selectedEstado.map(({ nmClassificacaoEn }) => nmClassificacaoEn) : selectedEstado.map(({ nmClassificacaoPt }) => nmClassificacaoPt);
-      t = [...new Set(t)];
-      setLstDistinct(t!);
-    } else if (isDistrictSel) {
-      setLstDadosCidade(selectedCidade);
-      let t = isEnglish ? selectedEstado.map(({ nmClassificacaoEn }) => nmClassificacaoEn) : selectedEstado.map(({ nmClassificacaoPt }) => nmClassificacaoPt);
-      t = [...new Set(t)];
-      setLstDistinct(t!);
-    }
-  }, [selectedEstado, selectedCidade]);
+    const selectedData = isStateSelected ? selectedEstado : isDistrictSelected ? selectedCidade : [];
+    setLstDados(selectedData);
+
+    const distinctNames = selectedData.map((item) => (isEnglish ? item.nmClassificacaoEn : item.nmClassificacaoPt));
+    const uniqueNames = [...new Set(distinctNames)];
+    setLstDistinct(uniqueNames);
+    console.log(selectedData);
+  }, [selectedEstado, selectedCidade, isStateSelected, isDistrictSelected]);
 
   return (
     <Box>
-      {lstDistinct.map((item) => (
-        <DataSection
-          key={`${item}`}
-          title={`${item}`}
-          propsEstado={isStateSel && lstDadosEstado ? lstDadosEstado.filter((i) => (isEnglish ? (i.nmClassificacaoEn == item) : (i.nmClassificacaoPt == item))) : undefined}
-          propsCidade={isDistrictSel && lstDadosCidade ? lstDadosCidade.filter((i) => (isEnglish ? (i.nmClassificacaoEn == item) : (i.nmClassificacaoPt == item))) : undefined}
-        />
-      ))}
+      {lstDistinct.map((item) => {
+        const filteredData = lstDados.filter((i) =>
+          isEnglish ? i.nmClassificacaoEn === item : i.nmClassificacaoPt === item,
+        ) as Estado[];
+        return <DataSection key={item} title={item} propsEstado={filteredData.length > 0 ? filteredData : undefined} />;
+      })}
       {selectedEstado ? <ComparisonSection /> : null}
     </Box>
   );
