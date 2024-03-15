52a7837d2911b6f412dbc8ff61264071ed996837
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-15 12:54:35 -0300

    Adds StatesDate property in TableContent.tsx

--- atlas-frontend/src/components/ComparisonMode/TableMode/TableContent/TableContent.tsx
+++ atlas-frontend/src/components/ComparisonMode/TableMode/TableContent/TableContent.tsx
@@ -12,6 +12,7 @@ import { Estado } from 'src/interfaces/Estado.type';
 
 interface Props<T> {
   comparison: T[];
+  estadosData: Array<Estado>;
 }
 interface DictionaryData {
   title: string;
@@ -19,20 +20,8 @@ interface DictionaryData {
   title_english: string; // Added property for English title
 }
 
-const TableContent: React.FC<Props<State> | Props<District>> = ({ comparison }) => {
-  const allEstados = useSelector(estadosSelected);
-  const filtredData: Estado[] = [];
+const TableContent: React.FC<Props<State> | Props<District>> = ({ comparison, estadosData }) => {
   const [dictionaryData, setDictionaryData] = useState<Array<DictionaryData>>([]);
-
-  const compareEstados = () => {
-    comparison.forEach((estado: State) => {
-      const data = allEstados.filter((estadoData) => estadoData.cdEstado == estado.properties.CD_UF);
-      for (let i = 0; i < data.length; i++) {
-        //console.log(`Data ${i}:`, data[i]);
-        filtredData.push(data[i]);
-      }
-    });
-  };
 
   useEffect(() => {
     async function fetchData() {
@@ -49,32 +38,9 @@ const TableContent: React.FC<Props<State> | Props<District>> = ({ comparison }) 
     fetchData();
   }, []);
 
-  const estadosData: { [key: number]: { estado: number; dados: Estado[] } } = {};
-
   useEffect(() => {
-    compareEstados();
-    filtredData.forEach((dado) => {
-      const estado = dado.cdEstado;
-      if (!estadosData[estado]) {
-        estadosData[estado] = {
-          estado: estado,
-          dados: [dado], // Inicia um array com o primeiro dado do estado
-        };
-      } else {
-        estadosData[estado].dados.push(dado); // Adiciona o dado ao array existente para o estado
-      }
-    });
-    //console.log('Estados Data:', estadosData);
-    dictionaryData.map((estado) => {
-      console.log(`${estado.title}`, estado);
-    });
-  }, [filtredData]);
-
-  useEffect(() => {
-    filtredData.map((objeto) => {
-      console.log('filtredData', objeto);
-    });
-  }, [filtredData]);
+    console.log('estadosData:', estadosData);
+  }, [estadosData]);
 
   const isEnglish =
     window.location.href.includes('/comparison_en') || window.location.href.includes('/comparison_states_en');
@@ -110,55 +76,9 @@ const TableContent: React.FC<Props<State> | Props<District>> = ({ comparison }) 
   const sortedSections = dictionaryData.sort((a, b) => {
     return sectionOrder.indexOf(a.title) - sectionOrder.indexOf(b.title);
   });
-  //Depuração
-  // useEffect(() => {
-  //   console.log('sortedSections', sortedSections);
-  // }, [sortedSections]);
 
   return (
     <>
-      {/* {sortedSections.map((section: DictionaryData) => (
-        <Collapsible
-          isTitle={true}
-          title={isEnglish ? section.title_english : section.title}
-          key={isEnglish ? section.title_english : section.title}
-        >
-          {section.content.map((content: MapPropsContentType, id) => (
-            <div key={id}>
-              {!content.nestedData &&
-              content.title !== 'População Estimada em 2017' &&
-              content.title !== 'População Estimada em 2018' &&
-              content.title !== 'População Estimada em 2019' &&
-              content.title !== 'População Estimada em 2020' &&
-              content.title !== 'Estimated Population in 2017' &&
-              content.title !== 'Estimated Population in 2018' &&
-              content.title !== 'Estimated Population in 2019' &&
-              content.title !== 'Estimated Population in 2020' &&
-              content.title !== 'População' &&
-              content.title !== 'Population' ? (
-                <Styles.Table lineTableNumber={id}>
-                  <Tooltip
-                    title={
-                      <div>
-                        <div>{isEnglish ? content.description : content.description}</div>
-                      </div>
-                    }
-                  >
-                    <Styles.ColumnTitle>{isEnglish ? content.title : content.title}</Styles.ColumnTitle>
-                  </Tooltip>
-                  {filtredData.map((objeto, index) => (
-                    <Styles.Column gridColumnNumber={index + 2} key={index}>
-                      <MetricDetails propsEstado={objeto} />
-                    </Styles.Column>
-                  ))}
-                </Styles.Table>
-              ) : (
-                <div key={id}></div>
-              )}
-            </div>
-          ))}
-        </Collapsible>
-      ))} */}
       {sortedSections.map((section: DictionaryData) => (
         <Collapsible
           isTitle={true}
@@ -188,7 +108,17 @@ const TableContent: React.FC<Props<State> | Props<District>> = ({ comparison }) 
                   >
                     <Styles.ColumnTitle>{isEnglish ? content.title : content.title}</Styles.ColumnTitle>
                   </Tooltip>
-                  {filtredData.map((estado, index) => (
+                  {/* {estadosData.map(
+                    (estado, index) => (
+                      console.log('ESTADO ENVIADO:', estado), // Add a closing parenthesis ')' after the console.log statement
+                      (
+                        <Styles.Column gridColumnNumber={index + 2} key={index}>
+                          <MetricDetails propsEstado={estado} />
+                        </Styles.Column>
+                      )
+                    ),
+                  )} */}
+                  {estadosData.map((estado, index) => (
                     <Styles.Column gridColumnNumber={index + 2} key={index}>
                       <MetricDetails propsEstado={estado} />
                     </Styles.Column>
