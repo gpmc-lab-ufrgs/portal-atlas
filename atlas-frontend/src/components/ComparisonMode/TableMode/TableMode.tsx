1ab42d97d0787e856bd3a0714c0fca6ab8f005e4
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-15 12:54:16 -0300

    Add useEffect hook and StatesDate parameter in TableMode.tsx

--- atlas-frontend/src/components/ComparisonMode/TableMode/TableMode.tsx
+++ atlas-frontend/src/components/ComparisonMode/TableMode/TableMode.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Box } from '@mui/material';
 
@@ -10,22 +10,31 @@ import Header from './Header';
 import TableContent from './TableContent';
 
 import * as Styles from './styles';
+import { Estado } from 'src/interfaces/Estado.type';
 
 interface Props<T> {
   comparison: T[];
+  estadosData: Estado[];
+  // cidadesData: Cidades[];
 }
 
 const isState = window.location.href.includes('/comparison_states');
 
-const TableMode: React.FC<Props<State> | Props<District>> = ({ comparison }) => {
+const TableMode: React.FC<Props<State> | Props<District>> = ({ comparison, estadosData }) => {
+  useEffect(() => {
+    console.log('estadosData:', estadosData);
+  }, [estadosData]);
+
   return (
     <Styles.TablerContainer>
       <Header comparison={comparison} />
-      <Box>
-        {districtProps.map((section, id) => (
-          <TableContent key={id} section={section} comparison={comparison} />
-        ))}
-      </Box>
+      {isState ? (
+        <Box>
+          <TableContent comparison={comparison} estadosData={estadosData} />
+        </Box>
+      ) : (
+        <div></div>
+      )}
     </Styles.TablerContainer>
   );
 };
