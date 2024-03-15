93fcd0ef24d71b5a88db34676f5451ca3e1d68d7
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-15 12:53:58 -0300

    Adds imports and state comparison logic

--- atlas-frontend/src/components/ComparisonMode/index.tsx
+++ atlas-frontend/src/components/ComparisonMode/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect } from 'react';
 
 import { useComparison } from '@context/comparisonContext';
 import { useComparison as useComparisonState } from '@context/comparisonContextState';
@@ -7,12 +8,43 @@ import GridMode from './GridMode';
 import TableMode from './TableMode';
 
 import * as Styles from './styles';
+import { useSelector } from 'react-redux';
+import { estadosSelected } from 'src/features/estadosSlice';
+import { Estado } from 'src/interfaces/Estado.type';
+
+import { Cidades } from 'src/interfaces/Cidades.type';
+import { State } from '@customTypes/state';
 
 interface Props {
   comparisonType: string;
 }
 
 const ComparisonMode: React.FC<Props> = ({ comparisonType }) => {
+  const allEstados = useSelector(estadosSelected);
+  const filtredData: Estado[] = [];
+
+  const compareEstados = () => {
+    isState
+      ? comparison.forEach((estado: State) => {
+          const data = allEstados.filter((estadoData) => estadoData.cdEstado == estado.properties.CD_UF);
+          for (let i = 0; i < data.length; i++) {
+            //console.log(`Data ${i}:`, data[i]);
+            filtredData.push(data[i]);
+          }
+        })
+      : comparison.forEach((estado: State) => {
+          const data = allEstados.filter((estadoData) => estadoData.cdEstado == estado.properties.CD_UF);
+          for (let i = 0; i < data.length; i++) {
+            //console.log(`Data ${i}:`, data[i]); //IMPLEMENTAR PARA CIDADES
+            filtredData.push(data[i]);
+          }
+        });
+  };
+
+  useEffect(() => {
+    compareEstados();
+  }, [filtredData]);
+
   let comparison: any;
 
   const isState =
@@ -27,7 +59,7 @@ const ComparisonMode: React.FC<Props> = ({ comparisonType }) => {
   function comparisonModeToggle() {
     switch (comparisonType) {
       case 'table':
-        return <TableMode comparison={comparison} />;
+        return <TableMode comparison={comparison} estadosData={filtredData} />;
       case 'grid':
         return <GridMode comparison={comparison} />;
       default:
